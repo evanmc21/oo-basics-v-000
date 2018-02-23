93253ea313da8e4ecc53f2f82681f920df43c3a2
Evan Candler <emcandler21@gmail.com>
2018-02-23 19:18:18 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,9 @@
 # Make your shoe class here!
 class Shoe
+  attr_accessor :Nike :color :size :material :condition
   def initialize(brand)
     @brand = brand
   end
+  def cobble
+    puts "Your shoe is as good as new!"
 end
